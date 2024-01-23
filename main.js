5b8008f89fd3beca2e9f4a3a590ca3dec6e288aa
AmanRaSingh <developer.aman.sing>
2024-01-23 14:07:58 +0530

    hello

---
+++ main.js
@@ -0,0 +1,14 @@
+// var a="hello world";
+// console.log(a);
+
+// var a = 4;
+// var b = 10;
+// var c = a + b
+// console.log(c)
+
+
+for (let i = 1; i <= 10; i++) {
+    // b = b * i
+    // console.log(b)
+    console.log(i)
+}
